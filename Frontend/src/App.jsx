1b5d689013782c9c75e9f346c15591690766f928
alan <alan.moex@gmail.com>
2024-05-21 21:33:10 -0300

    add container components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,16 @@
 
+
+
 function App() {
+
+  let patientsList = [{
+    name:"Pedro",
+    lastName:"Martinez",
+    dni:"24546375",
+    mail:"PedroMartinez@gmail.com",
+    password:"messi123",
+    role:"patient"
+  }];
 
   return (
     <>
