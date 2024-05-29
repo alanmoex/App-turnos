e979a1d16518e573468dde9a8c694dfeb893d095
alanmoex <alan.moex@gmail.com>
2024-05-29 20:27:50 -0300

    add register, login buttons. Also bg image

--- Frontend/src/components/common/navBar/navBar.jsx
+++ Frontend/src/components/common/navBar/navBar.jsx
@@ -1,32 +1,34 @@
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Button, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
-
-const navBar = () => {
+const NavBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar
+      bg="primary"
+      data-bs-theme="dark"
+      expand="lg"
+      className="bg-body-tertiary"
+    >
+      <div className="container-fluid">
+        <Navbar.Brand href="#home">MediCare</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+          <Nav className="ms-auto align-items-center">
+            <Nav.Link href="#home">
+              <Button>REGISTRARSE</Button>
+            </Nav.Link>
+            <Nav.Link href="#link">
+              <Button>INICIAR SESIÓN</Button>
+            </Nav.Link>
+            <NavDropdown title="USUARIO" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Cerrar Sesión
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+      </div>
     </Navbar>
   );
 };
 
-export default navBar;
+export default NavBar;
--- Frontend/src/components/container/homePage/HomePage.jsx
+++ Frontend/src/components/container/homePage/HomePage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import NavBar from "../../common/navBar/NavBar";
+import { Col, Row } from "react-bootstrap";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="container-fluid background-image">
+      <Row>
+        <NavBar></NavBar>
+      </Row>
+      <Row className="h-100">
+        <Col className="d-flex align-items-center justify-content-end">
+          <h1>MediCare</h1>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
