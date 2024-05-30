ec0a7fe44f08d36ffe0654de886e6913a64fdf9d
alanmoex <alan.moex@gmail.com>
2024-05-30 11:35:07 -0300

    add routes and logo

--- Frontend/src/components/common/navBar/navBar.jsx
+++ Frontend/src/components/common/navBar/navBar.jsx
@@ -1,6 +1,18 @@
-import { Button, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Button, Nav, NavDropdown, NavItem, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import logo from "../../../assets/logo.png";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const handleRegisterClick = () => {
+    navigate("/registerPatient");
+  };
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
   return (
     <Navbar
       bg="primary"
@@ -9,16 +21,25 @@ const NavBar = () => {
       className="bg-body-tertiary"
     >
       <div className="container-fluid">
-        <Navbar.Brand href="#home">MediCare</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            src={logo}
+            alt="Logo"
+            width="30"
+            height="30"
+            className="d-inline-block align-top me-2"
+          />
+          MediCare
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto align-items-center">
-            <Nav.Link href="#home">
-              <Button>REGISTRARSE</Button>
-            </Nav.Link>
-            <Nav.Link href="#link">
-              <Button>INICIAR SESIÓN</Button>
-            </Nav.Link>
+            <NavItem className="me-2">
+              <Button onClick={handleRegisterClick}>REGISTRARSE</Button>
+            </NavItem>
+            <NavItem className="me-2">
+              <Button onClick={handleLoginClick}>INICIAR SESIÓN</Button>
+            </NavItem>
             <NavDropdown title="USUARIO" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">
                 Cerrar Sesión
