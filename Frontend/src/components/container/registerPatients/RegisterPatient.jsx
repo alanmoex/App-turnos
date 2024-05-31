5900f5aa8129d6ed0a2e6be29c31d113687b4a96
AugustoMikbel <agussgabriel8@gmail.com>
2024-05-31 19:54:58 -0300

    no se conecta css

--- Frontend/src/components/container/registerPatients/RegisterPatient.jsx
+++ Frontend/src/components/container/registerPatients/RegisterPatient.jsx
@@ -1,9 +1,36 @@
-
+import "./RegisterPatient.css";
 
 const RegisterPatient = () => {
   return (
-    <div>RegisterPatient</div>
-  )
-}
+    <div className="registro-container">
+      <div className="registro-form">
+        <h2>Registrarse</h2>
+        <form>
+          <div className="form-group">
+            <label>Nombre</label>
+            <input type="text" name="nombre" />
+          </div>
+          <div className="form-group">
+            <label>Apellido</label>
+            <input type="text" name="apellido" />
+          </div>
+          <div className="form-group">
+            <label>Dni</label>
+            <input type="text" name="dni" />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label>Contraseña</label>
+            <input type="password" name="password" />
+          </div>
+          <button type="submit">Registrarse</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default RegisterPatient
+export default RegisterPatient;
