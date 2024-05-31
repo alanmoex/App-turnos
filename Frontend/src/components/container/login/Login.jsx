e41cc7949265e99ad3a888caa7246cb12c773d57
Julian Paquez <julianpaquez@gmail.com>
2024-05-31 19:54:10 -0300

    login resuelto no se conecta css

--- Frontend/src/components/container/login/Login.jsx
+++ Frontend/src/components/container/login/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Login.css";
 
-const Login = () => {
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (username === "" || password === "") {
+      alert("Por favor, complete todos los campos.");
+    } else {
+      alert("Has ingresado correctamente");
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="login-container">
+      <div className="login-form">
+        <h2>MediCare</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Usuario"
+              value={username}
+              onChange={(usernameLogin) =>
+                setUsername(usernameLogin.target.value)
+              }
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              id="password"
+              name="password"
+              placeholder="Contraseña"
+              value={password}
+              onChange={(passwordLogin) =>
+                setPassword(passwordLogin.target.value)
+              }
+            ></input>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
