eb8d3d824297dfecb3a53d0a2befe03e13f33687
alanmoex <alan.moex@gmail.com>
2024-06-04 15:01:13 -0300

    Add MedicalCenter

--- Backend/API-Turnos/Domain/Entities/MedicalCenter.cs
+++ Backend/API-Turnos/Domain/Entities/MedicalCenter.cs
@@ -2,5 +2,15 @@
 
 public class MedicalCenter
 {
+    public int Id { get; set;}
+    public string Name { get; set;}
+    public List<Appointment> Appointments { get; set;} = new List<Appointment>();
+    public List<Specialty> Specialties{ get; set;} = new List<Specialty>();  
+
+    public MedicalCenter(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
 
 }
