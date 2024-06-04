d94cf8d54582738dd1997c7ead19c883b9e7c7cf
alanmoex <alan.moex@gmail.com>
2024-06-04 15:00:40 -0300

    Add Appointment

--- Backend/API-Turnos/Domain/Entities/Appointment.cs
+++ Backend/API-Turnos/Domain/Entities/Appointment.cs
@@ -1,6 +1,23 @@
-﻿namespace Domain.Entities;
+﻿using System.Data;
+
+namespace Domain.Entities;
 
 public class Appointment
 {
+    public DateTime AppointmentDateTime { get; set; }
+
+    public Medic Medic { get; set; }
+
+    public Patient Patient{ get; set; }
+
+    public bool IsCancelled { get; set; }
+
+    public Appointment(DateTime appointmentDateTime, Medic medic, Patient patient)
+    {
+        AppointmentDateTime = appointmentDateTime;
+        Medic = medic;
+        Patient = patient;
+        IsCancelled = false;
+    }
 
 }
