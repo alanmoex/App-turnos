c5d4fdf281c7ecc3e9e31dfe06a268d6c1309947
alanmoex <alan.moex@gmail.com>
2024-06-04 15:00:25 -0300

    Add SysAdmin

--- Backend/API-Turnos/Domain/Entities/SysAdmin.cs
+++ Backend/API-Turnos/Domain/Entities/SysAdmin.cs
@@ -1,6 +1,12 @@
 ﻿namespace Domain.Entities;
 
-public class SysAdmin
+public class SysAdmin : User
 {
-
+    public SysAdmin(int id, string name, string email, string password)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Password = password;
+    }
 }
