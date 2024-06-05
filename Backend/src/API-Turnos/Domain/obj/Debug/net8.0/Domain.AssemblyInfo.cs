2ca39c20892b12719e25afe81ea7ad9edfdae49a
alanmoex <alan.moex@gmail.com>
2024-06-05 16:59:25 -0300

    agrega minuta y diagrama de clases

--- Backend/src/API-Turnos/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Backend/src/API-Turnos/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188bd48a3d07a1dafc1794da05472e46dfcf39e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d750fe6178e0df97c143e474ac1c086e8346c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
