b86362fda5a66828ead316d105986feb21eca957
alanmoex <alan.moex@gmail.com>
2024-06-12 16:05:55 -0300

    deleted medic list from specialties

--- Backend/src/API-Turnos/Domain/Entities/Specialty.cs
+++ Backend/src/API-Turnos/Domain/Entities/Specialty.cs
@@ -7,8 +7,6 @@ public class Specialty
     [Key]
     public int Id { get; set;}
     public string Name { get; set;}
-    public List<Medic>? Medics { get; set;}
-
     public Specialty( string name)
     {
         
