93bbc36fb1b071f2a6c620f7bb7d6d41cba88d17
alanmoex <alan.moex@gmail.com>
2024-06-16 11:49:52 -0300

    set patient and specialty repository to db

--- Backend/src/API-Turnos/Infrastructure/Data/SpecialtyRepository.cs
+++ Backend/src/API-Turnos/Infrastructure/Data/SpecialtyRepository.cs
@@ -6,34 +6,39 @@ namespace Infrastructure.Data;
 
 public class SpecialtyRepository : ISpecialtyRepository
 {
-    static int LastIdAssigned = 0;
-    static List<Specialty> specialties = [];
+    private readonly ApplicationContext _context;
+    public SpecialtyRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
 
     public Specialty Add(Specialty specialty)
     {
-        specialty.Id = ++LastIdAssigned;
-        specialties.Add(specialty);
+        _context.Specialties.Add(specialty);
+        _context.SaveChanges();
         return specialty;
     }
     public List<Specialty> GetAll()
     {
-        return specialties;
+        return _context.Specialties.ToList();
     }
 
     public Specialty? GetById(int id)
     {
-        return specialties.FirstOrDefault(x => x.Id == id);
+        return _context.Specialties.FirstOrDefault(x => x.Id == id);
     }
 
     public void Delete(Specialty specialty)
     {
-        specialties.Remove(specialty);
+        _context.Specialties.Remove(specialty);
+        _context.SaveChanges();
     }
 
     public Specialty Update(Specialty specialty)
     {
-        var obj = specialties.FirstOrDefault(p => p.Id == specialty.Id) ?? throw new Exception();
+        var obj = _context.Specialties.FirstOrDefault(p => p.Id == specialty.Id) ?? throw new Exception();
         obj.Name = specialty.Name;
+        _context.SaveChanges();
         return obj;
     }
 }
