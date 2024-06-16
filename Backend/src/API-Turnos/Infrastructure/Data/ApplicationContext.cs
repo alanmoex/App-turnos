4e20924d239ae76fd187d0e118105f00ea86f922
alanmoex <alan.moex@gmail.com>
2024-06-16 11:48:03 -0300

    create context and set db

---
+++ Backend/src/API-Turnos/Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AdminMC> AdminMCs { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Medic> Medics { get; set; }
+        public DbSet<MedicalCenter> MedicalCenters { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Specialty> Specialties { get; set; }
+        public DbSet<SysAdmin> SysAdmins { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<WorkSchedule> WorkSchedules { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
