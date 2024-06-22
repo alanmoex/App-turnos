308d6f54c44175d8a230346b327412c51a1aaee4
alanmoex <alan.moex@gmail.com>
2024-06-22 09:05:17 -0300

    add licensenumber to medic dto

--- Backend/src/API-Turnos/WebApplication1/Program.cs
+++ Backend/src/API-Turnos/WebApplication1/Program.cs
@@ -47,6 +47,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
